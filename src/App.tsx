e4dbeade838224ece38e8ddd3fc35abe29ac7c51
pilot2912 <shashankgoel2912@gmail.com>
2026-04-30 12:46:49 +0530

    Push source files for initial project setup on 2026-04-30 07:12:53.

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Counter from './pages/Counter'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/counter" element={<Counter />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
